6b9e42ee4cc69247e4111b878515845c8504c444
Kelvin Shisanya <Kelvin.Shisanya@DDDATA.KE3>
2025-03-14 19:04:15 +0300

    update

--- js/dark.js
+++ js/dark.js
@@ -4,7 +4,7 @@ const body = document.body;
 // Check if user has a preference
 if (localStorage.getItem("theme") === "dark") {
     body.classList.add("dark-mode");
-    toggleButton.textContent = "Mode ";
+    toggleButton.textContent = "Off ";
 }
 
 // Toggle Function
@@ -14,9 +14,9 @@ toggleButton.addEventListener("click", () => {
     // Save User Preference
     if (body.classList.contains("dark-mode")) {
         localStorage.setItem("theme", "dark");
-        toggleButton.textContent = "Mode ";
+        toggleButton.textContent = "Off ";
     } else {
         localStorage.setItem("theme", "light");
-        toggleButton.textContent = "Mode";
+        toggleButton.textContent = "On";
     }
 });
